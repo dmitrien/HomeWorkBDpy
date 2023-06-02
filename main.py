fe22183200555824242c6671e2c73e691c68eddc
Dmitrienko Artem <admitienko@mail.ru>
2023-06-02 17:16:21 +0300

    fix bugs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import psycopg2
+
+
 def create_table():
     cur.execute("""CREATE TABLE userinfo(
                 id SERIAL PRIMARY KEY,
@@ -13,13 +15,15 @@ def create_table():
                 number VARCHAR UNIQUE constraint mobnumber_PhoneNumber check (number not like '%[^0-9]%')
                 );
                 """)
-    return conn.commit()
+    pass
+
 
 def create_user(cursor, name: str, last_name: str, email: str):
     cursor.execute("""
     INSERT INTO userinfo(name, last_name, email) VALUES (%s, %s, %s);\
     """, (name, last_name, email))
-    return conn.commit()
+    pass
+
 
 def get_id_user(email: str):
     cur.execute("""
@@ -28,68 +32,83 @@ def get_id_user(email: str):
     user_id = cur.fetchone()
     return user_id[0]
 
+
 def add_number(cursor, number: int, email: str):
     usersid = get_id_user(email)
     cursor.execute("""
     INSERT INTO mobnumber(user_id, number) VALUES (%s, %s);\
     """, (usersid, number,))
-    return conn.commit()
+    pass
+
 
 def edite_user(cursor, useremail, name=None, last_name=None, email=None):
     usersid = get_id_user(useremail)
-    cursor.execute("""
-    UPDATE userinfo SET name=%s, last_name=%s, email=%s WHERE id=%s;\
-    """, (name, last_name, email, usersid,))
-    return conn.commit()
+    if name!=None:
+        cursor.execute("""
+        UPDATE userinfo SET name=%s WHERE id=%s;\
+        """, (name, usersid,))
+    if last_name!=None:
+        cursor.execute("""
+        UPDATE userinfo SET last_name=%s WHERE id=%s;\
+        """, (last_name, usersid,))
+    if email!=None:
+        cursor.execute("""
+        UPDATE userinfo SET email=%s WHERE id=%s;\
+        """, (email, usersid,))
+    pass
+
 
 def delete_number(cursor, email: str):
     usersid = get_id_user(email)
     cursor.execute("""
     DELETE FROM mobnumber WHERE user_id=%s;\
     """, (usersid,))
-    return conn.commit()
+    pass
 
 
 def delete_user(cursor, email: str):
     usersid = get_id_user(email)
+    delete_number(cursor, email)
     cursor.execute("""
                 DELETE FROM userinfo WHERE id=%s;\
                 """, (usersid,))
-    return conn.commit()
+    pass
 
-def search_user(cursor):
-    name = input("Введите имя ")
-    last_name = input("Введите фамилию ")
-    email = input("Введите email ")
-    number = input("Введите номер телефона ")
-    cur.execute("""
-    SELECT name, last_name, email FROM userinfo
-    WHERE email=%s OR name=%s OR last_name=%s;
-    """, (email, name, last_name))
-    cur.execute("""
-    SELECT name, last_name, email FROM userinfo u
-    JOIN mobnumber m ON m.user_id=u.id
-    WHERE m.number=%s;
-    """, (number,))
+
+def search_user(cursor, name=None, last_name=None, email=None, number=None):
+    if name!=None:
+        cursor.execute("""
+            SELECT name, last_name, email FROM userinfo
+            WHERE name=%s;
+            """, (name,))
+    if last_name!=None:
+        cursor.execute("""
+            SELECT name, last_name, email FROM userinfo
+            WHERE last_name=%s;
+            """, (last_name,))
+    if email!=None:
+        cursor.execute("""
+            SELECT name, last_name, email FROM userinfo
+            WHERE email=%s;
+            """, (email,))
+    if number!=None:
+        cursor.execute("""
+        SELECT name, last_name, email FROM userinfo u
+        JOIN mobnumber m ON m.user_id=u.id
+        WHERE m.number=%s;
+        """, (number,))
     user_info = cur.fetchall()
     return print("Вот такую информацию получили по вашему запросу ", user_info)
 
-with psycopg2.connect(database='netology_db', user='postgres', password='') as conn:
-    with conn.cursor() as cur:
-        # create_table()
-        # create_user(cur, "Егор", "Самойлов", "pewewer@yahsk.df")
-        # add_number(cur, 89643647159, "sobaka@mail.com")
-        # edite_user(cur, "Олеган", "Пупкин", "sobak657a@mail.com", "perdun33@yashik.du")
-        # delete_number(cur, "sobaka@mail.com")
-        # delete_user(cur, "sobaka@mail.com")
-        search_user(cur)
 
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    with psycopg2.connect(database='netology_db', user='postgres', password='') as conn:
+        with conn.cursor() as cur:
+            # create_table()
+            # create_user(cur, "Петр", "Примеров", "pew5ew45er@ya7sk.df")
+            # add_number(cur, 89645647199, "pewewer@yahsk.df")
+            # edite_user(cur, "pewewer@yahsk.df", "Пупкинec", "Тестович")
+            # delete_number(cur, "sobaka@mail.com")
+            # delete_user(cur, "pewewer@yahsk.df")
+            # search_user(cur, 'Петр',)
+    conn.close()
